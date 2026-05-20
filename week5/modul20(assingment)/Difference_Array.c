3928f23ffb2d873b6a14ac22ddd53769f6012626
shuvo-xi <shuvokhan77779@gmail.com>
2026-05-20 20:19:59 +0600

    1

---
+++ week5/modul20(assingment)/Difference_Array.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        int a[n],b[n],c[n];
+        for(int i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+            b[i]=a[i];
+        }
+         for(int i=0;i<n-1;i++)
+        {
+             for(int j=i+1;j<n;j++)
+             {
+                if(b[i]>b[j])
+                {
+                    int temp=b[i];
+                    b[i]=b[j];
+                    b[j]=temp;
+                }
+             }
+             
+        }
+        for(int i=0;i<n;i++)
+        {
+            c[i]=a[i]-b[i];
+            printf("%d ",abs(c[i]));
+        }
+        printf("\n");
+    }
+   
+    return 0;
+}
