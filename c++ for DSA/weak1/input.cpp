d4824fa9bb4f5d0365ba33b468b8f9f7b8a4a2c9
shuvo-xi <shuvokhan77779@gmail.com>
2026-05-21 21:58:10 +0600

    1

---
+++ c++ for DSA/weak1/input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    // std::cin>> x;
+    // std::cout<< x << std::endl;
+    std::cin >> x;
+    std::cout << x << std::endl;
+    char c = 'b';
+    // cout << (int)c << endl;
+    int ascii=c;
+    cout << ascii << endl;
+    return 0;
+}
---
+++ c++ for DSA/weak1/print.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{
+    int x=10;
+    char y='s';
+    double z=56.57;
+    std::cout<< "\nhello world:" << x << " " << y << " " <<std::endl << z;
+    std::cout << "\nhello world";
+    return 0;
+}
