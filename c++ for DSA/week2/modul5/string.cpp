52a816327687628442ed115e67479295c1d30ff1
shuvo-xi <shuvokhan77779@gmail.com>
2026-06-11 00:30:45 +0600

    1

---
+++ c++ for DSA/week2/modul5/string.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char s1[10]="Hello";
+    char s2[10]="hello";
+    if(strcmp(s1,s2)==0)
+    {
+        cout<< "same" <<endl;
+    }
+    else{
+        cout<< "not_same" <<endl;
+    }
+    return 0;
+}
