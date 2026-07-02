f966f36ac8c427230b3329864ecaf0203fedf408
shuvo-xi <shuvokhan77779@gmail.com>
2026-07-02 22:28:33 +0600

    1

---
+++ Basic Data stracture/week_2/modul5/daynamic_node.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class node
+{
+public:
+    int val;
+    node *next;
+    node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+int main()
+{
+    node* head =new node (10);
+     node* a =new node (20);
+      node* b =new node (30);
+    head->next=a;
+    a->next=b;                                                              
+
+    cout <<head->val  << endl;
+    cout << head->next->val << endl;
+    return 0;
+}
