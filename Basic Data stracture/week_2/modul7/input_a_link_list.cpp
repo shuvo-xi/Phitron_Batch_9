29acde89fc695bb162620522930f59298537bddc
shuvo-xi <shuvokhan77779@gmail.com>
2026-07-12 20:27:57 +0600

    1

---
+++ Basic Data stracture/week_2/modul7/input_a_link_list.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newnode;
+        tail=newnode;
+        return;
+    }
+    tail->next = newnode;
+    tail = newnode;
+}
+void print_link_list(Node *head)
+{
+    Node *tem = head;
+    while (tem != NULL)
+    {
+        cout << tem->val << endl;
+        tem = tem->next;
+    }
+}
+int main()
+{
+
+    Node*head=NULL;
+    Node*tail=NULL;
+    int val;
+    while(true)
+    {
+        cin>>val;
+        if(val==-1)
+        {
+            break;
+        }
+         insert_at_tail(head, tail, val);
+    }
+    print_link_list(head);
+    // Node *head = new Node(10);
+    // Node *a = new Node(20);
+    // Node *tail = new Node(30);
+
+    // head->next = a;
+    // a->next = tail;
+    // insert_at_tail(head, tail, 100);
+    // print_link_list(head);
+    return 0;
+}
+
