1a3647d247c71d74ff559ed84e3bfb19f18fe0a7
shuvo-xi <shuvokhan77779@gmail.com>
2026-07-23 20:08:53 +0600

    1

--- Basic Data stracture/week_2/modul7/DIividing_Candies.cpp
+++ Basic Data stracture/week_2/modul7/DIividing_Candies.cpp
@@ -1,1 +1,44 @@
+ #include <bits/stdc++.h>
  
+ using namespace std;
+ class Node
+ {
+ public:
+     int val;
+     Node *next;
+     Node(int val)
+     {
+         this->val = val;
+         this->next = NULL;
+     }
+ };
+ int main()
+ {
+     int t; cin>>t;
+     while(t--)
+     {
+        int n,x; cin>>n>>x;
+        vector<int> a(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+
+        }
+        vector<int>can_take;
+          for(int i=0;i<n;i++)
+        {
+            if(a[i]%x==0)
+            {
+                can_take.push_back(a[i]);                                     
+            }
+
+        }
+        int largest=0;
+        for(int i=0;i<can_take.size();i++)
+        {
+            largest=max(largest,can_take[i]);
+        }
+        cout<<largest<<endl;
+     }
+     return 0;
+ }
